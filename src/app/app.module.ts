97e5c65578e789dd91120cb1ec957fe6203942f1
Julien Contreras <julien.contreras@actoll.com>
2018-01-10 16:29:52 +0100

    ajout des trades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {NoopInterceptor} from '@angular/common/http/src/interceptor';
 import {AuthentificationInterceptor} from './hitbtc/auth/authentification.interceptor';
 import {AuthentificationService} from './hitbtc/auth/authentification.service';
-import {TradingService} from './hitbtc/api/trading.api';
+import {TradingApiService} from './hitbtc/api/trading.api';
 import {GlobalInfoService} from './hitbtc/api/global-info.api';
 import {Stock} from './wallet/stock';
 import {WalletService} from './wallet/wallet.service';
@@ -15,6 +15,7 @@ import {DecimalPipe} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import { AlertModule } from 'ngx-bootstrap';
 import {StockService} from "./wallet/stock.service";
+import {TradesService} from "./wallet/trades.service";
 
 
 @NgModule({
@@ -33,9 +34,10 @@ import {StockService} from "./wallet/stock.service";
     multi: true,
   },
     AuthentificationService,
-    TradingService,
+    TradingApiService,
     GlobalInfoService,
     WalletService,
+    TradesService,
     StockService,
     DecimalPipe],
   bootstrap: [AppComponent]
